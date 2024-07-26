faf7e389c18433e4941010d65feb1b789883ad7b
ndy <ratcomp9992@gmail.com>
2024-07-26 17:57:49 +0900

    Init: initial project

---
+++ src/main/java/io/openAPI_test/OpenApiTestApplication.java
@@ -0,0 +1,13 @@
+package io.openAPI_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenApiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/openAPI_test/OpenApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.openAPI_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
