bafe2e53cd8e89e172ef03704cef977c3ccea961
ndy <ratcomp9992@gmail.com>
2024-07-28 10:51:31 +0900

    Feat: VO 추가

---
+++ src/main/java/io/openAPI_test/domain/model/CookRcp01.java
@@ -0,0 +1,21 @@
+package io.openAPI_test.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
+public class CookRcp01 {
+    @JsonProperty("total_count")
+    private Integer totalCount;
+
+    @JsonProperty("row")
+    private List<RecipeVo> recipeVoList;
+}
