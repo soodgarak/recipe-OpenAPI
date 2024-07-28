7b8dc78c3a1d23bfcd347a8ad098c1b62636dc78
ndy <ratcomp9992@gmail.com>
2024-07-28 17:45:17 +0900

    Feat: Recipe DB 저장 기능 구현

--- src/main/java/io/openAPI_test/service/RecipeService.java
+++ src/main/java/io/openAPI_test/service/RecipeService.java
@@ -5,8 +5,11 @@ import io.openAPI_test.domain.Recipe;
 import io.openAPI_test.domain.RecipeApi;
 import io.openAPI_test.repository.RecipeRepo;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
+import java.util.Iterator;
 import java.util.List;
 
 @Service
@@ -14,6 +17,13 @@ import java.util.List;
 public class RecipeService {
     private final RecipeRepo recipeRepo;
 
+    private List<Recipe> insertRecipe(List<Recipe> recipeList) {
+        recipeRepo.saveAll(recipeList);
+
+        return recipeList;
+    }
+
+    //private Recipe findRecipe()
     public List<Recipe> convertStringToRecipe(String recipeWithString) {
         RecipeApi recipeApi = null;
         try {
@@ -24,6 +34,25 @@ public class RecipeService {
             e.printStackTrace();
         }
 
-        return recipeApi.getRecipeApiDetail().getRecipeList();
+        return insertRecipe(recipeApi.getRecipeApiDetail().getRecipeList());
     }
+
+//    private RecipeApi convertStringToManualAndManualImage(String recipeWithString) {
+//
+//        JSONObject jsonObject = new JSONObject(recipeWithString);
+//
+//        JSONObject recipeApiDetail = jsonObject.getJSONObject("COOKRCP01");
+//        JSONArray recipeList = recipeApiDetail.getJSONArray("row");
+//        for (int i = 0; i < recipeList.length(); i++) {
+//            JSONObject recipe = recipeList.getJSONObject(i);
+//
+//            Iterator<String> keys = recipe.keys();
+//            while(keys.hasNext()) {
+//                String key = keys.next();
+//                if (key.startsWith("MANUAL") && key.contains("IMG") && recipe.getString(key) != null) {
+//
+//                }
+//            }
+//        }
+//    }
 }
