29523f829703da4b7188d9dc8f0894dfc27ca08b
ndy <ratcomp9992@gmail.com>
2024-07-28 13:08:06 +0900

    Chore: @JsonIgnoreProperties 추가

--- src/main/java/io/openAPI_test/domain/model/RecipeApi.java
+++ src/main/java/io/openAPI_test/domain/model/RecipeApi.java
@@ -1,9 +1,11 @@
 package io.openAPI_test.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class RecipeApi {
     @JsonProperty("COOKRCP01")
     private RecipeApiDetail recipeApiDetail;
