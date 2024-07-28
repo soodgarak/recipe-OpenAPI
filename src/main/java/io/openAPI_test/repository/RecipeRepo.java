80f49100afa8dd196a1c09cea6d1fa025c035106
ndy <ratcomp9992@gmail.com>
2024-07-28 10:51:48 +0900

    Feat: Repository 추가

---
+++ src/main/java/io/openAPI_test/repository/RecipeRepo.java
@@ -0,0 +1,7 @@
+package io.openAPI_test.repository;
+
+import io.openAPI_test.domain.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecipeRepo extends JpaRepository<Recipe, Long> {
+}
