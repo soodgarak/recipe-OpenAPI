5c639200b2cf1187c82520fa6f0a9464778f0386
ndy <ratcomp9992@gmail.com>
2024-07-29 22:56:03 +0900

    Chore: Manual, ManualImage 통합

--- src/main/java/io/openAPI_test/domain/Manual.java
+++ src/main/java/io/openAPI_test/domain/Manual.java
@@ -8,13 +8,15 @@ public class Manual {
     @EmbeddedId
     private ManualId manualId;
     private String manual;    // 만드는 법
+    private String manualImgUrl; // 만드는 법 이미지 URL
 
-    private Manual (Recipe recipe, Long manualId, String manual) {
+    private Manual (Recipe recipe, Long manualId, String manual, String manualImgUrl) {
         this.manualId = new ManualId(recipe.getId(), manualId);
         this.manual = manual;
+        this.manualImgUrl = manualImgUrl;
     }
 
-    public static Manual of(Recipe recipe, Long manualId, String manual) {
-        return new Manual(recipe, manualId, manual);
+    public static Manual of(Recipe recipe, Long manualId, String manual, String manualImgUrl) {
+        return new Manual(recipe, manualId, manual, manualImgUrl);
     }
 }
